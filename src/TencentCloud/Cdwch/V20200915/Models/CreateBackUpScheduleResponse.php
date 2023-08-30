24aa2d168884a446a4fa186b8a8579e471f9e380
tencentcloudapi <tencentcloudapi@tencent.com>
2023-08-30 08:20:34 +0800

    release 3.0.965

--- src/TencentCloud/Cdwch/V20200915/Models/CreateBackUpScheduleResponse.php
+++ src/TencentCloud/Cdwch/V20200915/Models/CreateBackUpScheduleResponse.php
@@ -20,17 +20,25 @@ use TencentCloud\Common\AbstractModel;
 /**
  * CreateBackUpSchedule返回参数结构体
  *
+ * @method string getErrorMsg() 获取错误描述
+ * @method void setErrorMsg(string $ErrorMsg) 设置错误描述
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class CreateBackUpScheduleResponse extends AbstractModel
 {
     /**
+     * @var string 错误描述
+     */
+    public $ErrorMsg;
+
+    /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
+     * @param string $ErrorMsg 错误描述
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -46,6 +54,10 @@ class CreateBackUpScheduleResponse extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("ErrorMsg",$param) and $param["ErrorMsg"] !== null) {
+            $this->ErrorMsg = $param["ErrorMsg"];
+        }
+
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
